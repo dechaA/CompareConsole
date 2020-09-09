8a08340e0e96f7bbff3cf3eb03a79caa775901fe
dechaA <43436469+dechaA@users.noreply.github.com>
2020-09-09 14:33:07 +0700

    Update Module1.vb

--- Module1.vb
+++ Module1.vb
@@ -6,6 +6,7 @@ Module Module1
     Public Sub Main() 'ByVal Method As String, Optional ByVal ConfigureFile As String = "", Optional ByVal ExtensionPatterns As String = "")
         Console.WriteLine("Test .Net Framework 4.5")
         Console.WriteLine("Test Automate")
+        Console.WriteLine("Fixed Bugs")
     End Sub
 
 End Module
